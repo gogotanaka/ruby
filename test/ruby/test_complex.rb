24aa021e7916037e1eddf2a3b5489b308690601e
gogotanaka <mail@tanakakazuki.com>
2015-03-15 01:23:31 -0700

    Add tests

--- test/ruby/test_complex.rb
+++ test/ruby/test_complex.rb
@@ -966,6 +966,10 @@ class Complex_Test < Test::Unit::TestCase
       assert_in_delta(1.092, c.real, 0.001)
       assert_in_delta(-0.420, c.imag, 0.001)
 
+      c = CMath.log2(Complex(1, 2))
+      assert_in_delta(1.160, c.real, 0.001)
+      assert_in_delta(1.597, c.imag, 0.001)
+
       c = CMath.log10(Complex(1, 2))
       assert_in_delta(0.349, c.real, 0.001)
       assert_in_delta(0.480, c.imag, 0.001)
